1f32c5a46fecbc44547900aa405f24ea9fd33498
ofir.mamo <ofir.mamo@ermetic.com>
2022-01-15 23:00:46 +0200

    Done almost

--- pre_processing/main.py
+++ pre_processing/main.py
@@ -8,7 +8,19 @@ from lifeforms_gen import rle_decoder as decoder
 matplotlib.use('TkAgg')
 
 board = sg.Board(size=(64, 64))
-with open("../data-sources/oscillators/1beacon.rle") as f:
+with open("../data-sources/oscillators/38p7.2.rle") as f:
+    raw_content = f.read()
+    raw_blinker = decoder.decode(raw_content)
+    board.add(lifeform=lf.Custom(raw_blinker), loc=(30, 30))
+
+sim = sg.Simulator(board)
+stats = sim.run(sg.rules.conway_classic, iters=1000)
+anim = sim.animate()
+plt.show()
+
+
+board = sg.Board(size=(64, 64))
+with open("../data-sources/oscillators/eureka.rle") as f:
     raw_content = f.read()
     raw_blinker = decoder.decode(raw_content)
     board.add(lifeform=lf.Custom(raw_blinker), loc=(30, 30))
